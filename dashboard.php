c7365e34bba5160ee6c526a624e3dbfbfbbe1938
Andrey Z <zayarniy@gmail.com>
2026-03-15 03:45:58 +0300

    Выложил на сервер

--- dashboard.php
+++ dashboard.php
@@ -554,6 +554,5 @@ foreach ($schedule as $lesson) {
     </script>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <? include 'footer.php'?>
 </body>
 </html>
--- diaries.php
+++ diaries.php
@@ -1555,8 +1555,6 @@ if ($publicView) {
         }
     </script>
 
-
-<?php include 'footer.php'; ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>    
 </body>
 
